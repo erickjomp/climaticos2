6912e2d1ff558b355bed33cbc856afc8f21a556c
DARC1199 <diego.rojas.c@uni.pe>
2025-10-14 22:27:48 -0500

    Add files via upload
    Changes made looking for an uniform downscaling method

--- code/06_bias_correction_PET_CORDEX.R
+++ code/06_bias_correction_PET_CORDEX.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(hyfo)
 library(climQMBC)
+# library(MBC)
+# library(qmap)
 Sys.setenv(TZ = 'GMT')
 
 #### INPUT ####
@@ -12,40 +14,50 @@ file_periods_analysis <-
 
 file_serie_PET_pisco <-
   "output/00_processed_data/df_ETP_m.rds"
- 
+
 
 #### OUTPUT ####
 file_series_PET_CORDEX_BC__rds <-
-  "output/06_bias_correction_CORDEX_series//df_PET_CORDEX_m_BC.rds"
+  "output/06_bias_correction_CORDEX_series/df_PET_CORDEX_m_BC.rds"
 file_series_PET_CORDEX_BC__csv <-
-  "output/06_bias_correction_CORDEX_series//df_PET_CORDEX_m_BC.csv"
+  "output/06_bias_correction_CORDEX_series/df_PET_CORDEX_m_BC.csv"
 
 #### reading INPUT ####
 df_PET_CORDEX <- readRDS(file_series_PET_CORDEX)
 
+df_PET <- readRDS(file_serie_PET_pisco) 
+
 df_periods_analysis <- read.csv(file_periods_analysis,
                                 header = FALSE,
-                                row.names = 1) %>% t() %>% 
-  as_tibble() %>%   mutate_all(as.POSIXct) 
-print(t(df_periods_analysis))
+                                row.names = 1) %>% t() %>% as_tibble()
 
-df_PET <- readRDS(file_serie_PET_pisco) # PISCO PET will be observed PET
+H_inicio <- as.POSIXct(df_periods_analysis$date_begin_historic,format="%d/%m/%Y")
+H_final <- as.POSIXct(df_periods_analysis$date_end_historic,format="%d/%m/%Y")
+F_inicio <- as.POSIXct(df_periods_analysis$date_begin_ref_future,format="%d/%m/%Y")
+F_final <- as.POSIXct(df_periods_analysis$date_end_ref_future,format="%d/%m/%Y")
 
 #### Bias Correction PET ####
-df_PET__obs <- df_PET %>% 
-  filter(dates >= df_periods_analysis$date_begin_historic_PET,
-         dates < df_periods_analysis$date_end_historic_PET)
 
-df_PET_CORDEX <- df_PET_CORDEX %>% 
-  filter(dates >= df_periods_analysis$date_begin_historic_PET,
-         dates < df_periods_analysis$date_end_ref_future)
+df_PET_obs <- df_PET %>% 
+  filter(dates >= H_inicio,
+         dates < H_final)
+
+df_PET_H_CORDEX <- df_PET_CORDEX %>% 
+  filter(dates >= H_inicio,
+         dates < H_final)
+
+df_PET_F_CORDEX <- df_PET_CORDEX %>% 
+  filter(dates >= F_inicio,
+         dates < F_final)
+
+df_PET_M_CORDEX <- rbind(df_PET_H_CORDEX,df_PET_F_CORDEX)
 
 # loop 
 df_PET_CORDEX_BC <- 
-df_PET_CORDEX %>% group_by(RCM, GCM) %>% 
+  df_PET_M_CORDEX %>% group_by(RCM, GCM) %>% 
   group_split() %>% 
   lapply(function(df_PET_CORDEX_model){
-    message(" | ", appendLF = FALSE)
+    # message(" | ", appendLF = FALSE)
     ## arrange done to be able to use SDM function of climQMBC package
     
     list_BC <- list()
@@ -55,9 +67,7 @@ df_PET_CORDEX %>% group_by(RCM, GCM) %>%
         filter(scenario  %in% c("historical", sce_rcp))
       
       # climQMBC
-      values_BC <- climQMBC::QDM(obs = df_PET__obs$etp,
-                                 mod = df_PET_CORDEX_model_sce$PET,
-                                 var = 1,frq = "m")
+      values_BC <- climQMBC::DQM(obs=df_PET_obs$etp,mod=df_PET_CORDEX_model_sce$PET,mult_change=1,allow_negatives=0,frq = "m")
       
       df_PET_CORDEX_model_sce_BC <- df_PET_CORDEX_model_sce
       df_PET_CORDEX_model_sce_BC$PET <- values_BC
@@ -72,19 +82,5 @@ df_PET_CORDEX %>% group_by(RCM, GCM) %>%
 
 #### writing output ####
 saveRDS(df_PET_CORDEX_BC ,file_series_PET_CORDEX_BC__rds)
-write.csv(df_PET_CORDEX_BC, file_series_PET_CORDEX_BC__csv, 
+write.csv(df_PET_CORDEX_BC, file_series_PET_CORDEX_BC__csv,
           quote = FALSE, row.names = FALSE)
-
-
-#### extra plots ####
-
-df_PET_CORDEX_BC %>% 
-  ggplot(aes(dates, PET, col = scenario)) + 
-  geom_line() +
-  facet_wrap(~name) +
-  ggthemes::theme_few() + 
-  labs(x = NULL, y = "PET (mm/month)") +
-  geom_line(data = df_PET__obs %>% rename(PET= etp) %>% 
-              mutate(dates = as.Date(dates)), 
-            col = "black")
-  
--- code/06_bias_correction_pr_CORDEX.R
+++ code/06_bias_correction_pr_CORDEX.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(hyfo)
 library(climQMBC)
+
 Sys.setenv(TZ = 'GMT')
 
 #### INPUT ####
@@ -19,21 +20,27 @@ file_serie_pr_pisco <-
 
 #### OUTPUT ####
 file_series_pr_CORDEX_BC__rds <-
-  "output/06_bias_correction_CORDEX_series//df_pr_all_CORDEX_m_BC.rds"
+  "output/06_bias_correction_CORDEX_series/df_pr_all_CORDEX_m_BC.rds"
 file_series_pr_CORDEX_BC__csv <-
-  "output/06_bias_correction_CORDEX_series//df_pr_all_CORDEX_m_BC.csv"
+  "output/06_bias_correction_CORDEX_series/df_pr_all_CORDEX_m_BC.csv"
 
 #### reading INPUT ####
 df_pr_CORDEX <- readRDS(file_series_pr_CORDEX)
 
 df_periods_analysis <- read.csv(file_periods_analysis,
                                 header = FALSE,
-                                row.names = 1) %>% t() %>% 
-  as_tibble() %>%   mutate_all(as.POSIXct) 
+                                row.names = 1) %>% t() %>% as_tibble()
+
+H_inicio <- as.POSIXct(df_periods_analysis$date_begin_historic,format="%d/%m/%Y")
+H_final <- as.POSIXct(df_periods_analysis$date_end_historic,format="%d/%m/%Y")
+F_inicio <- as.POSIXct(df_periods_analysis$date_begin_ref_future,format="%d/%m/%Y")
+F_final <- as.POSIXct(df_periods_analysis$date_end_ref_future,format="%d/%m/%Y")
+
+# %>%   mutate_all(as.POSIXct)
 print(t(df_periods_analysis))
 
 df_pr_inters <- readRDS(file_series_pr_inters)
-df_pr_pisco <- readRDS(file_serie_pr_pisco) 
+df_pr_pisco <- readRDS(file_serie_pr_pisco)
 
 
 #### processing ####
@@ -46,43 +53,47 @@ list_results_BC <- list()
 for (source_pr in names(df_pr_all)[-1]){
   message(which(source_pr == names(df_pr_all)[-1]), " / ",
           ncol(df_pr_all) -1, "\r", appendLF = FALSE ) 
+
+  df_pr_obs <- df_pr_all %>% dplyr::select(dates, pr = !!source_pr) %>%
+    filter(dates >= H_inicio,
+           dates < H_final)
   
-  df_pr__obs <- df_pr_all %>% select(dates, pr = !!source_pr) %>% 
-    filter(dates >= df_periods_analysis$date_begin_historic_pr,
-           dates < df_periods_analysis$date_end_historic_pr)
+  df_pr_H_CORDEX <- df_pr_CORDEX %>% 
+    filter(dates >= H_inicio,
+           dates < H_final)
   
-  df_pr_CORDEX <- df_pr_CORDEX %>% 
-    filter(dates >= df_periods_analysis$date_begin_historic_pr,
-           dates < df_periods_analysis$date_end_ref_future)
+  df_pr_F_CORDEX <- df_pr_CORDEX %>% 
+    filter(dates >= F_inicio,
+           dates < F_final)
+  
+  df_pr_M_CORDEX <- rbind(df_pr_H_CORDEX,df_pr_F_CORDEX)
   
   # loop 
   df_pr_CORDEX_BC <- 
-    df_pr_CORDEX %>% group_by(RCM, GCM) %>% 
+    df_pr_M_CORDEX %>% group_by(RCM, GCM) %>% 
     group_split() %>% 
     lapply(function(df_pr_CORDEX_model){
-      # message(" | ", appendLF = FALSE)
-      ## arrange done to be able to use SDM function of climQMBC package
-      
       list_BC <- list()
       for (sce_rcp in c("rcp45", "rcp85")){
         df_pr_CORDEX_model_sce <- 
           df_pr_CORDEX_model %>% 
           filter(scenario  %in% c("historical", sce_rcp))
         
-        # climQMBC
-        values_BC <- climQMBC::DQM(obs = df_pr__obs$pr,
-                                   mod = df_pr_CORDEX_model_sce$pr,
-                                   var = 1,frq = "m")
+        # ClimQMBC
+        values_BC <- climQMBC::DQM(obs=df_pr_obs$pr,mod=df_pr_CORDEX_model_sce$pr,mult_change=1,allow_negatives=0,frq = "m")
         
         df_pr_CORDEX_model_sce_BC <- df_pr_CORDEX_model_sce
         df_pr_CORDEX_model_sce_BC$pr <- values_BC
         
         list_BC[[sce_rcp]] <- df_pr_CORDEX_model_sce_BC
       }
+      
       # since both are the same hist are same we join#
       df_pr_CORDEX_model_BC <- list_BC %>% do.call(rbind,.) %>% 
         filter(!duplicated(dates) | (scenario != "historical"))
+      
       return(df_pr_CORDEX_model_BC)
+      
     }) %>% do.call(rbind,.)
   
   df_pr_CORDEX_BC <- df_pr_CORDEX_BC %>% mutate(source_pr = source_pr, 
@@ -95,58 +106,5 @@ df_pr_all_CORDEX_BC <- list_results_BC %>% do.call(rbind,.)
   
 #### writing output ####
 saveRDS(df_pr_all_CORDEX_BC ,file_series_pr_CORDEX_BC__rds)
-write.csv(df_pr_all_CORDEX_BC, file_series_pr_CORDEX_BC__csv, 
+write.csv(df_pr_all_CORDEX_BC, file_series_pr_CORDEX_BC__csv,
           quote = FALSE, row.names = FALSE)
-
-
-#### extra plots ####
-
-df_pr_all_CORDEX_BC %>% 
-  filter(source_pr == "IED") %>% 
-  ggplot(aes(dates, pr, col = scenario)) + 
-  geom_line() +
-  facet_wrap(~name) +
-  ggthemes::theme_few() + 
-  labs(x = NULL, y = "Precipitation (mm/month)") +
-  geom_line(data = df_pr__obs %>% 
-              mutate(dates = as.Date(dates)), 
-            col = "black")
-
-df_pr_all_CORDEX_BC %>% 
-  filter(source_pr == "IED") %>% 
-  ggplot(aes(dates, pr, col = scenario)) + 
-  geom_line() +
-  facet_wrap(~name) +
-  ggthemes::theme_few() + 
-  labs(x = NULL, y = "Precipitation (mm/month)") 
-
-df_pr_CORDEX %>% 
-  ggplot(aes(dates, pr, col = scenario)) + 
-  geom_line() +
-  facet_wrap(~name) +
-  ggthemes::theme_few() + 
-  labs(x = NULL, y = "Precipitation (mm/month)") 
-
-
-## ploting just historic of 1 model
-df_pr_all_CORDEX_BC %>% filter(source_pr == "IDW") %>% 
-  filter(name == "SMHI-RCA4_CCCma-CanESM2", scenario == "historical") %>% 
-  select(dates, pr) %>% plot(type = "l", ylim = c(0,250))
-
-df_pr_all %>% filter(dates>=df_periods_analysis$date_begin_historic_pr,
-                     dates < df_periods_analysis$date_end_historic_pr) %>% 
-  mutate(dates = as.Date(dates)) %>% 
-  select(dates, IDW) %>% lines(col = "red")
-
-
-## same
-df_pr_all_CORDEX_BC %>% filter(source_pr == "IDW") %>% 
-  filter(name == "SMHI-RCA4_CCCma-CanESM2", 
-         scenario %in% c("historical", "rcp45")) %>% 
-  select(dates, pr) %>% plot(type = "l", ylim = c(0,250))
-
-df_pr_all %>% filter(dates>=df_periods_analysis$date_begin_historic_pr,
-                     dates < df_periods_analysis$date_end_historic_pr) %>% 
-  mutate(dates = as.Date(dates)) %>% 
-  select(dates, IDW) %>% lines(col = "red")
-
